6198e2ed39438e38738ccfbd104cf89a1f0d3f94
Xeonus <vanderbourg@gmail.com>
2023-01-22 12:04:39 +0100

    update query timings

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -49,8 +49,9 @@ export async function splitQuery<Type>(
 
 export function useDeltaTimestamps(): [number, number, number] {
   const utcCurrentTime = dayjs()
-  const t1 = utcCurrentTime.subtract(1, 'day').startOf('minute').unix()
-  const t2 = utcCurrentTime.subtract(2, 'day').startOf('minute').unix()
-  const tWeek = utcCurrentTime.subtract(1, 'week').startOf('minute').unix()
+  //startOf changed to hour from minute
+  const t1 = utcCurrentTime.subtract(1, 'day').startOf('hour').unix()
+  const t2 = utcCurrentTime.subtract(2, 'day').startOf('hour').unix()
+  const tWeek = utcCurrentTime.subtract(1, 'week').startOf('hour').unix()
   return [t1, t2, tWeek]
 }
