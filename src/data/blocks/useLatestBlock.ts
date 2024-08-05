72df593c8974f17c6798241e1d4b6e513e7fd5ae
Xeonus <vanderbourg@gmail.com>
2024-08-05 10:52:27 +0200

    refactor: increase block poll interval
    - reduce queries amount on idle page by increasing poll interval from 10s to 3 mins

--- src/data/blocks/useLatestBlock.ts
+++ src/data/blocks/useLatestBlock.ts
@@ -2,7 +2,7 @@ import { useGetLatestBlockQuery } from '../../apollo/generated/graphql-codegen-g
 import { ApolloClient, NormalizedCacheObject } from '@apollo/client';
 
 export function useLatestBlock(blockClient: ApolloClient<NormalizedCacheObject>): { blockNumber?: number; loading: boolean } {
-    const { data, loading } = useGetLatestBlockQuery({ pollInterval: 10000, client: blockClient });
+    const { data, loading } = useGetLatestBlockQuery({ pollInterval: 180000, client: blockClient });
 
     return {
         blockNumber: data?.blocks[0]?.number ? parseFloat(data.blocks[0].number) : undefined,
