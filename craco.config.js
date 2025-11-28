24c3215c0a13e5431c6f44f7bb6f11452f93b4b7
Xeonus <vanderbourg@gmail.com>
2025-11-28 14:12:03 +0100

    Major Security Patches: November 2025
    - craco upgrades
    - resolving all med to high severity issues
    - upgrading firebase tooling

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,41 @@ module.exports = {
   typescript: {
     enableTypeChecking: false
   },
+  devServer: (devServerConfig) => {
+    // webpack-dev-server v5 compatibility patch for react-scripts 5.0.1
+    // Several options were renamed or restructured in v5:
+    // - onBeforeSetupMiddleware/onAfterSetupMiddleware -> setupMiddlewares
+    // - https -> server
+    const {
+      onBeforeSetupMiddleware,
+      onAfterSetupMiddleware,
+      https,
+      ...rest
+    } = devServerConfig;
+
+    const config = {
+      ...rest,
+      setupMiddlewares: (middlewares, devServer) => {
+        if (onBeforeSetupMiddleware) {
+          onBeforeSetupMiddleware(devServer);
+        }
+        if (onAfterSetupMiddleware) {
+          onAfterSetupMiddleware(devServer);
+        }
+        return middlewares;
+      },
+    };
+
+    // Convert https option to server option if present
+    if (https) {
+      config.server = {
+        type: 'https',
+        options: typeof https === 'object' ? https : {},
+      };
+    }
+
+    return config;
+  },
   webpack: {
     configure: (webpackConfig, { env }) => {
       // Handle ESM/CommonJS issues for development
